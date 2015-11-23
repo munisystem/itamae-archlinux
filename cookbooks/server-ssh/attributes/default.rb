5f5ca1603109b1598ad51c1851975d9798fc2a2b
munisystem <munisystem@gmail.com>
2015-11-23 14:10:25 +0900

    Add cookbook 'server-ssh'

---
+++ cookbooks/server-ssh/attributes/default.rb
@@ -0,0 +1,8 @@
+node.reverse_merge!(
+  server_ssh: {
+    port: 22
+  },
+  user: [
+    'muni'
+  ]
+)
---
+++ cookbooks/server-ssh/default.rb
@@ -0,0 +1,26 @@
+include_recipe './attributes/default.rb'
+
+service 'sshd' do
+  action :nothing
+end
+
+template '/etc/ssh/sshd_config' do
+  notifies :restart, 'service[sshd]', :immediately
+end
+
+node['user'].each do |user|
+
+  directory "/home/#{user}/.ssh" do
+    owner user
+    group user
+    mode "700"
+  end
+
+  template "/home/#{user}/.ssh/authorized_keys" do
+    source "./templates/authkeys/#{user}.pub"
+    owner user
+    group user
+    mode "600"
+  end
+
+end
