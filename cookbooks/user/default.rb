4a66ee41b9afbda9c7a3d36f9fb4cc7bee053c31
munisystem <munisystem@gmail.com>
2015-11-24 13:59:51 +0900

    Add sudo settings - user

--- cookbooks/user/default.rb
+++ cookbooks/user/default.rb
@@ -1,5 +1,13 @@
 include_recipe './attributes/default.rb'
 
+package 'sudo'
+
+template '/etc/sudoers' do
+  source './templates/sudoers'
+  user 'root'
+  mode '0440'
+end
+
 node['user'].each do |user|
 
   user "Add general user @#{user}" do
