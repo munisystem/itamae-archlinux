7dacaf0345cedcfee5b8c5173be4b9fe8f3ec7eb
munisystem <munisystem@gmail.com>
2015-12-01 10:54:08 +0900

    Fix bug 'docker'

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -5,13 +5,3 @@ end
 service 'docker' do
   action :nothing
 end
-
-template '/etc/systemd/network/ipforward.network' do
-  source './templates/ipforward.network'
-  notifies :restart, 'service[systemd-networkd]', :immediately
-  notifies :restart, 'service[docker]', :immediately
-end
-
-service 'systemd-networkd' do
-  action :nothing
-end
